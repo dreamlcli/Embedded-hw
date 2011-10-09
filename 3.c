cd15ed4a70adfd9d441b88eb8ab2f7c41facadfc
peterlee <peterlee0127@gmail.com>
2011-10-09 11:31:46 +0800

    3.c

--- 3.c
+++ 3.c
@@ -4,10 +4,12 @@ int main()
 	short n1,n2,n3,Min;
 	scanf("%hx, %hx, %hx",&n1,&n2,&n3);
 
-	
-        
-            
-        
+    if(n1<n2 && n1<n3)
+        {Min = n1;}
+    if(n2<n1 && n2<n3)
+        {Min = n2;}
+    if(n3<n1 && n3<n2)
+        {Min = n3;}
 
 	printf("Min=0x%hx ",Min);	
 
